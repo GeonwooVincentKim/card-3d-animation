28e76162d2598cb3ada96f3d8f8ac95dd0ade6a2
GeonwooVincentKim <kdsnop@gmail.com>
2024-04-01 22:27:12 +0900

    [Update] - Replace file location

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:card_3d_animation/feature/drawer_3d.dart';
+import 'package:card_3d_animation/screens/home.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const My3dAnimationApp());
@@ -10,30 +12,16 @@ class My3dAnimationApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
+      title: "Card 3D Animation",
       theme: ThemeData(
         primarySwatch: Colors.indigo
       ),
       routes: {
-        "/": (context) => Home(),
+        "/": (context) => const Drawer3D(), 
+        "/home": (context) => const Home(),
       },
       onGenerateRoute: (settings) {
       },
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  const Home({super.key});
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
     );
   }
 }
